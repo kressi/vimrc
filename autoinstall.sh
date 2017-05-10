a2a56e556a30862d411921eda2fafa6bc8764bc9
kressi <mkressibucher@hotmail.com>
2017-05-10 09:57:27 +0200

    Change git urls to https

--- autoinstall.sh
+++ autoinstall.sh
@@ -20,7 +20,7 @@ die() {
 [ -e "~/.vim" ] && die "~/.vim already exists."
 [ -e "~/.vimrc" ] && die "~/.vimrc already exists."
 
-git clone --recursive git://github.com/kressi/vimrc.git "$VIMHOME"
+git clone --recursive https://github.com/kressi/vimrc.git "$VIMHOME"
 cd "$VIMHOME"
 git submodule update --init
 
